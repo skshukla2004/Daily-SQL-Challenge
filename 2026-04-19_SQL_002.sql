4c0ecf2fdfee3686b3d30bda24645024427d6e78
Shashikant Shukla <sks7523830807@gmail.com>
2026-04-20 16:59:34 +0530

    Add files via upload

---
+++ 2026-04-19_SQL_002.sql
@@ -0,0 +1,12 @@
+-- 100 Days SQL Practice
+-- Phase 1 — Basics (Day 1–20)
+
+-- Day 2
+-- Select only these columns from Product:
+ --ProductModelID SellStartDate ModifiedDate
+ select
+      ProductModelID,
+      SellStartDate,
+      ModifiedDate
+ from product;
+
