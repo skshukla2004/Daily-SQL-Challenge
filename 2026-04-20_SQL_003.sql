ef3fb01270a33b38dbd98a92a7e1e1fee15a30f7
Shashikant Shukla <sks7523830807@gmail.com>
2026-04-20 17:01:31 +0530

    Add files via upload

---
+++ 2026-04-20_SQL_003.sql
@@ -0,0 +1,7 @@
+-- 100 Days SQL Practice
+-- Phase 1 — Basics (Day 1–20)
+
+--Day 3
+--Retrieve all records from WorkOrder where OrderQty is greater than 50.
+Select * from WorkOrder
+Where OrderQty > 50;
