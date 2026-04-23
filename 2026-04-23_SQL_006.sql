2d9aaa75d74b3b4d43ce02134b330c703337b6a8
Shashikant Shukla <sks7523830807@gmail.com>
2026-04-23 18:26:18 +0530

    Update 2026-04-23_SQL_006.sql

--- 2026-04-23_SQL_006.sql
+++ 2026-04-23_SQL_006.sql
@@ -5,4 +5,4 @@
 -- Find all records from TransactionHistory where Quantity is between 10 and 100.
 select *
 from TransactionHistory 
-where Quantity between 10 and 100
+where Quantity between 10 and 100;
