8b6d495f22b38d562947cf21197a9cce7beb5d44
Shashikant Shukla <sks7523830807@gmail.com>
2026-04-23 18:25:44 +0530

    Add files via upload

---
+++ 2026-04-23_SQL_006.sql
@@ -0,0 +1,8 @@
+--100 Days SQL Practice
+--Phase 1 Basics (Day 1-20)
+
+-- Day 6
+-- Find all records from TransactionHistory where Quantity is between 10 and 100.
+select *
+from TransactionHistory 
+where Quantity between 10 and 100
