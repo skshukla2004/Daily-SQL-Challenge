1437902d95ed13c366ab83374b3404048ca59adc
Shashikant Shukla <sks7523830807@gmail.com>
2026-05-04 00:59:56 +0530

    Create 2026-04-26_SQL_009.sql

---
+++ 2026-04-26_SQL_009.sql
@@ -0,0 +1,8 @@
+--100 Days SQL Practice
+--Phase 1 Basics (Day 1-20)
+
+-- Day 9
+-- Display all records from WorkOrder ordered by DueDate in ascending order.
+   Select *
+   from WorkOrder 
+   order by DueDate asc ;
